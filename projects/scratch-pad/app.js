e57d8ce85bc8248ab39af846176a2285fc3824a7
Gary Marino <garymarinojr@gmail.com>
2019-04-02 01:55:48 +0000

    A basic website

---
+++ projects/scratch-pad/app.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+'use strict';
+
+function concat(stringOne, stringTwo) {
+    // YOUR CODE BELOW HERE //
+
+    return stringOne + stringTwo;
+
+    // YOUR CODE ABOVE HERE //
+}
